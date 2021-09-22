8b8ddc3531705918e8e294fb8928bf6dbf9c095d
Renato Caetano <renatocsare@gmail.com>
2021-09-22 14:18:00 -0300

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -36,6 +36,7 @@ var skillVoiceES = [{ lang: 'es-ES' }]
  * @param {string} name Name by which the chat widget should be referred
  */
 var initSdk = function(name) {
+	alert(sessionStorage.getItem('givenName'));
     if (!name) {
         name = 'Bots';          // Set default reference name to 'Bots'
     }
