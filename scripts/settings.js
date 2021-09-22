c9e50e78b3e52783b53e0d92a224602498371619
Renato Caetano <renatocsare@gmail.com>
2021-09-22 15:58:53 -0300

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -108,7 +108,7 @@ var initSdk = function(name) {
 			botButtonIcon: 'botOracle/images/iconConceptStore.png',
 			logoIcon: 'botOracle/images/iconConceptStore.png',
 			botIcon: 'botOracle/images/iconConceptStore.png', 	
-			font: '12px "Mier B", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+			font: '14px "Mier B", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
             		colors: {"branding": "#232323", "text": "#292929", "textLight": "#737373", "typingIndicator": "#D47229", 'botText': 'white', 'botMessageBackground': '#D47229', "actionsBackground": '#232323' },
 			i18n: {
 				"pt-BR": {
